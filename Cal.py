240eb0c247e01f9437551864080674e42afc1fcc
Thirumoorthi R <thirumoorthi.r@outlook.com>
2016-09-14 16:05:24 +0530

    Create Cal.py

---
+++ Cal.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python:
+# -*- coding: utf-8 -*-
+import facebook
+import time,os
+img_date= time.strftime("%d%m")#This line outputs date format as DDMM,we want this on our code to wget image files from the URL
+year=time.strftime("%Y")#this line outputs year Value
+url= "http://srirangaminfo.com/cal/"+year+"/"+img_date+".jpg"# here we are using sring concatination so that we can add two strings
+
+newfile = "C:\\Users\\592186\\Documents\\Python_study\\"+"image.jpg"#creating new file location as a variable
+os.remove(newfile)
+
+import wget
+wget.download(url)#using Wget we are downloading pic from that page mentioned in url variable 
+oldfile = "C:\\Users\\592186\\Documents\\Python_study"+"\\"+img_date+".jpg"#creating old file location as an variable
+
+try:
+  os.rename(oldfile,newfile)
+except:
+  print (" Seams like file already downloaded")
