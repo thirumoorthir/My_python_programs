08b5226f5b9e0ba780922858b1a3eb8620e2cd9f
Thirumoorthi R <thirumoorthi.r@outlook.com>
2016-11-24 19:21:48 +0530

    Update Cal.py

--- Cal.py
+++ Cal.py
@@ -6,12 +6,12 @@ img_date= time.strftime("%d%m")#This line outputs date format as DDMM,we want th
 year=time.strftime("%Y")#this line outputs year Value
 url= "http://srirangaminfo.com/cal/"+year+"/"+img_date+".jpg"# here we are using sring concatination so that we can add two strings
 
-newfile = "C:\\Users\\592186\\Documents\\Python_study\\"+"image.jpg"#creating new file location as a variable
+newfile = "C:\\Users\\\\Documents\\Python_study\\"+"image.jpg"#creating new file location as a variable
 os.remove(newfile)
 
 import wget
 wget.download(url)#using Wget we are downloading pic from that page mentioned in url variable 
-oldfile = "C:\\Users\\592186\\Documents\\Python_study"+"\\"+img_date+".jpg"#creating old file location as an variable
+oldfile = "C:\\Users\\\\Documents\\Python_study"+"\\"+img_date+".jpg"#creating old file location as an variable
 
 try:
   os.rename(oldfile,newfile)
